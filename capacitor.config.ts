4fb6f7b7b5cb3ca245f71d3ced9e0fae6eff2bc1
ShrineXY <yudhadobar2@gmail.com>
2026-09-05 23:26:42 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kasmu.app',
+  appName: 'Kasmu',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  android: {
+    backgroundColor: '#15120F'
+  }
+};
+
+export default config;
